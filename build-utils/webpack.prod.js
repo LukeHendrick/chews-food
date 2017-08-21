e32a6b59c274acd9e84347452efd95eb39c2bc2f
Luke Hendrick <lukehendrick41@gmail.com>
2017-08-21 10:57:22 -0500

    Adjusted styling

--- build-utils/webpack.prod.js
+++ build-utils/webpack.prod.js
@@ -2,6 +2,8 @@ const webpack = require('webpack');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
 const UglifyJsWebpackPlugin = require('uglifyjs-webpack-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin')
+const commonPaths = require('./common-paths');
 
 module.exports = {
     devtool: "source-map",
@@ -19,18 +21,19 @@ module.exports = {
     plugins: [
         new ExtractTextWebpackPlugin("styles.css"),
         new UglifyJsWebpackPlugin({ sourceMap: true }),
-        new CompressionWebpackPlugin({
-            asset: "[path].gz[query]",
-            algorithm: "gzip",
-            test: /\.(js|html|css)$/,
-            threshold: 10240,
-            minRatio: 0.8 
-        }),
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify('production')
             }
         }),
-        new webpack.optimize.AggressiveMergingPlugin()
+        new ManifestPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
+        new CompressionWebpackPlugin({
+            asset: "[path].gz[query]",
+            algorithm: "gzip",
+            test: /\.(js|html|css)$/,
+            threshold: 1000,
+            minRatio: 0.8
+        }),
     ]
 }
