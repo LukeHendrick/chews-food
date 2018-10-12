c74f9804d5fb7770a7ad3d7046a1e1e53c79ac99
Luke Hendrick <lukehendrick41@gmail.com>
2018-10-12 18:29:27 -0500

    Removed stuff

--- api.js
+++ api.js
@@ -4,7 +4,6 @@ const token = `Bearer ${process.env.MY_YELP_TOKEN}`
 module.exports = {
     yelpGet: (query) => {
         var url = "https://api.yelp.com/v3/businesses/search";
-        var token = 'Bearer aHiwuqAN04KexWk4nudWsUNnqi3qYJXuVunPytCTc3EH7fRF4RoCu_Rkr5EpUJrl533Qsc1wUfv36MM0dF7i-58T900Ju8wt06L0-mwNktFV7Vlgm4uZWZAsvGJ3WXYx';
         return axios.get(url, { 'params': query, 'headers': { 'Authorization': token} })
             .then((res) => {
                 return res.data.businesses;
